1d29fcb52e0146a9ff0cb68be2da83806ec81548
Tom Randus <tom.randus@gmail.com>
2016-04-26 15:27:53 +0200

    Update Ch 1. code - es5 + es6

--- Kapitola_1_Babel/src/index.js
+++ Kapitola_1_Babel/src/index.js
@@ -1,10 +1,14 @@
-var x = {
-  name: "Viléme"
-};
+//Zkušební kód
+//document.write("Ahoj kamarádi")
 
-var y = {
-    ...x,
+//Zkušební ES6 kód
+ var x = {
+   name: "Viléme"
+ };
+
+ var y = { //Spread Object direktiva sečte dva objekty
+     ...x,
     greeting: "Ahoj"
-};
+ }; 
 
-document.write(y.greeting + " " + y.name);
+ document.write(y.greeting + " " + y.name);
